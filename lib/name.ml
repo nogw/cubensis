bd33d371eb2add2b2bef0a9f146df0181079b3ea
nogw <gabrielnogueiraoliveira@gmail.com>
2022-11-14 18:50:29 -0300

    impl fresh name

--- lib/name.ml
+++ lib/name.ml
@@ -6,3 +6,10 @@ let compare = String.compare
 
 module NameMap = Map.Make (String)
 module NameSet = Set.Make (String)
+
+let ( <@> ) s1 s2 = NameSet.union s1 s2
+
+let rec fresh ~avoid ~name =
+  if (NameSet.mem name avoid) 
+    then fresh ~avoid ~name:(name ^ "\'")
+    else name
